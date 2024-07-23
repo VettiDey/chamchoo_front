64261e02075f230c7d45ab330cb01ffbd8b9083c
5vermind <5vermind@gmail.com>
2024-07-23 23:39:34 +0900

    handle tag

--- app/InputNickname.tsx
+++ app/InputNickname.tsx
@@ -1,32 +1,51 @@
-"use client";
-import { button } from "@nextui-org/theme";
-import { Input } from "@nextui-org/input";
-import { Link } from "@nextui-org/link";
-import { useState } from "react";
+'use client'
+import { button } from '@nextui-org/theme'
+import { Input } from '@nextui-org/input'
+import { Link } from '@nextui-org/link'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 
-import { siteConfig } from "@/config/site";
+import { siteConfig } from '@/config/site'
+
+const handleId = (id: string) => {
+  if (id.includes('#')) {
+    return id.replace('#', '-')
+  }
+
+  return `${id}-KR1`
+}
 
 export const InputNickname = () => {
-  const [nickname, setNickname] = useState("");
+  const [nickname, setNickname] = useState('')
+  const router = useRouter()
 
   return (
     <>
       <div>
-        <Input label="닉네임" value={nickname} onValueChange={setNickname} />
+        <Input
+          label="닉네임"
+          value={nickname}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              router.push(siteConfig.route.champion(handleId(nickname)))
+            }
+          }}
+          onValueChange={setNickname}
+        />
       </div>
 
       <div className="flex gap-3">
         <Link
           className={button({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
+            color: 'primary',
+            radius: 'full',
+            variant: 'shadow'
           })}
-          href={siteConfig.route.champion(nickname)}
+          href={siteConfig.route.champion(handleId(nickname))}
         >
           분석하기
         </Link>
       </div>
     </>
-  );
-};
+  )
+}
---
+++ app/champion/[id]/page.tsx
@@ -0,0 +1,28 @@
+import Analysis from './analysis'
+
+import { fetcher } from '@/lib/fetcher'
+import { AnalysisResult } from '@/types/api'
+
+interface Props {
+  params: {
+    id: string
+  }
+}
+
+export default async function ChampionPage({ params }: Props) {
+  const { id } = params
+  const data = await getAnalysisResult(id)
+
+  return <Analysis data={data} />
+}
+
+const getAnalysisResult = async (id: string) => {
+  const nickname = id.split('-')[0]
+  const tagline = id.split('-')[1]
+
+  console.log(`/analysis-result/${nickname}/?tagline=${tagline}`)
+
+  return await fetcher<AnalysisResult>(
+    `/analysis-result/${nickname}/?tagline=${tagline}`
+  )
+}
